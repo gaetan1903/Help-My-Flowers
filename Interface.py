3c20bdbea2c99646fb80480a904b017558cdf933
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2019-05-13 10:27:28 +0300

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -92,7 +92,7 @@ class Interface(threading.Thread):
 
 			server = smtplib.SMTP('smtp.gmail.com:587')
 			server.starttls()
-			server.login('gaetan.jonathan.bakary@esti.mg','keyloGGer1903')
+			server.login('gaetan.jonathan.bakary@esti.mg','**************')
 			server.send_message(message)
 			server.quit()
 
